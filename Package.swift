06cab82d3bc21b74ca73127f0542a5383ba93f59
Oleg Shishin <oleg.shishin@escape-team.tech>
2023-11-21 19:19:06 +0300

    dependency fix

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
           .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
           .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.21.0"),
           .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.2.1")),
-          .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.1")),
+          .package(url: "https://github.com/oshishin1/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.1")),
           .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
           .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
         ],
